55282cda856e3d7130811b5b13dc2d1e78932e32
wesam <wesam.a.s196@gmail.com>
2024-07-22 23:25:26 +0300

    add title and icon in head

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,12 +4,12 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>Index - MyResume Bootstrap Template</title>
+  <title>Wesam Salem</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/favicon.png" rel="icon"  >
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Fonts -->
@@ -36,7 +36,7 @@
   ======================================================== -->
 </head>
 
-<body class="index-page">
+<body class="index-page" >
 
   <header id="header" class="header d-flex flex-column justify-content-center">
 
