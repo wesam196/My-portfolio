341ce5cc8b0fa3c3e8985a4fe1b3484204466d92
wesam <wesam.a.s196@gmail.com>
2024-08-05 01:11:34 +0300

    edit database name

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,8 @@ use App\Http\Controllers\BackendWork;
 Route::get('/',[BackendWork::class,'index']);
 
 Route::get('/project/{id}',[BackendWork::class,'projectDetails']);
-Route::get('/createForm',[BackendWork::class,'create']);
-Route::post('/add_poject', [BackendWork::class , 'addProject']);
-Route::get('/read', [BackendWork::class , 'read']);
-Route::get('/delete/{id}', [BackendWork::class , 'delete']);
-Route::get('/update/{id}', [BackendWork::class , 'update']);
-Route::post('/confirmUpdate/{id}', [BackendWork::class , 'comfirmUpdate']);
-Route::get('/imagesGallary/{id}', [BackendWork::class , 'imageGallary']);
-Route::post('/addImage/{id}', [BackendWork::class , 'addimage']);
-Route::get('deleteImage/{id}' , [BackendWork::class , 'deleteImage']);
+
+
 
 
 Route::middleware([
@@ -26,4 +19,18 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::get('/register', function () {
+        if (Auth::check()) {
+            return redirect('/'); // Redirect to the homepage or another page.
+        }});
+    Route::get('/createForm',[BackendWork::class,'create']);
+Route::post('/add_poject', [BackendWork::class , 'addProject']);
+Route::get('/read', [BackendWork::class , 'read']);
+Route::get('/delete/{id}', [BackendWork::class , 'delete']);
+Route::get('/update/{id}', [BackendWork::class , 'update']);
+Route::post('/confirmUpdate/{id}', [BackendWork::class , 'comfirmUpdate']);
+Route::get('/imagesGallary/{id}', [BackendWork::class , 'imageGallary']);
+Route::post('/addImage/{id}', [BackendWork::class , 'addimage']);
+Route::get('deleteImage/{id}' , [BackendWork::class , 'deleteImage']);
 });
